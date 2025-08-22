58c9590950caae7ad1496f1df982a8406d2270c1
Deepanshu Tyagi <deepanshut041@gmail.com>
2025-08-22 01:53:50 -0400

    Add basic C++ programs for introduction and user input
    This commit adds four introductory C++ programs: a "Hello World" program, a user input output example, an integer sum computation program, and a division with quotient and remainder demonstration. These programs serve as a starting point for basic C++ understanding.

---
+++ basics/03_input_sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout << "Enter A: " << ends;
+    cin >> a;
+
+    cout << "Enter B: " << ends;
+    cin >> b;
+
+    cout << "Sum is: " << a + b << endl;
+
+    return 0;
+}
---
+++ basics/04_divison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout << "Enter a: " << ends;
+    cin >> a;
+
+    cout << "Enter b: " << ends;
+    cin >> b;
+
+    cout << "Quotient: " << a / b << endl;
+    cout << "Remainder: " << a % b << endl;
+}
